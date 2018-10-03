756fbe4c65a6c786fff0e704104d6a0e2f0de704
Kebairia_Zakaria <4.kebairia@gmail.com>
2018-10-03 19:46:15 +0100

    adding the pomodoro script

---
+++ pomodoro/pomodoro.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Import the color library
+# --- CONSTANTS --------------------------------------------------------
+reset=$(tput setf 0)
+blue=$(tput setf 1)
+green=$(tput setf 2)
+cyan=$(tput setf 3)
+red=$(tput setf 4)
+magenta=$(tput setf 5)
+yellow=$(tput setf 6)
+grey=$(tput setf 7)
+#----------------------
+br_black=$(tput setf 8)
+br_red=$(tput setf 9)
+br_green=$(tput setf 10)
+br_yellow=$(tput setf 11)
+br_blue=$(tput setf 12)
+br_magenta=$(tput setf 13)
+br_cyan=$(tput setf 14)
+white=$(tput setf 15)
+#----------------------
+#TODO: Creat the interface
+info() {
+echo ""
+echo "######################################################"
+echo "# This script is a pomodoro tech implementation"
+echo "# for an easy use during coding/scripting/studying ...etc"
+echo "######################################################"
+}
+notifyme() {
+    notify-send $1
+
+}
+focus(){
+    min=0
+    min_empty=24
+    for min in {0..24}
+    do
+	for sec in {0..59}
+	do
+	   _fill=$(printf "%${min}s")
+	   _left=$(printf "%${min_empty}s")
+	   printf "\r${br_yellow}PROGRESS:${br_red}│${_fill// /\█}${_left}│${white} ${min}m:${sec}s"
+	    sleep 0.01
+	done
+	let min_empty=$min_empty-1
+    done
+    echo
+}
+defus() {
+    min=0
+    min_empty=9
+    for min in {0..9}
+    do
+	for sec in {0..59}
+	do
+	   _fill=$(printf "%${min}s")
+	   _left=$(printf "%${min_empty}s")
+	   printf "\r${br_yellow}PROGRESS:${br_green}│${_fill// /\█}${_left}│${white}\t\t     ${min}m:${sec}s"
+	    sleep 0.01
+	done
+	let min_empty=$min_empty-1
+    done
+    echo
+}
+for i in {1..4}
+do
+    focus
+    notify-send "The end of focus time N°:${i}"
+    defus
+done
+
+
